33fbbd74e85f2261430a71bb8668e8376d3ebcda
Logan Bell <loganbell@gmail.com>
2015-08-25 10:56:21 -0700

    locallib

--- .grim_build.sh
+++ .grim_build.sh
@@ -11,6 +11,7 @@ cd "./$CLONE_PATH"
 curl -L https://cpanmin.us/ -o cpanm
 chmod +x cpanm
 
+./cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 ./cpanm --installdeps .
 
 perl Makefile.PL && make test dist
